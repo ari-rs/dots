c17ecff0b256dcca07da5a4357271bb2446282c1
ari <ari.eimer@proton.me>
2024-11-25 01:30:25 -0600

    nvim init

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+
+
+vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
+vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+vim.opt.number = true
+
+vim.opt.termguicolors = true
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
