711822093035eab9be1a954012a5365360d369a0
ari <ari.eimer@proton.me>
2024-12-19 23:48:22 -0600

    ddd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 function fish_greeting
 	#fastfetch
 end
-alias ls="exa --icons"
+alias ls="eza --icons"
+alias cat="bat"
+alias cd="__zoxide_z"
+alias cdi="__zoxide_zi"
+alias hx="helix"
 export EDITOR=nvim
 #function fish_prompt
 #    echo "e"
@@ -14,3 +18,15 @@ function y
 	end
 	rm -f -- "$tmp"
 end
+
+# https://unix.stackexchange.com/questions/235704/fish-sudo-command-not-found
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+        echo sudo $history[1]
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
+
+zoxide init --no-cmd fish | source
